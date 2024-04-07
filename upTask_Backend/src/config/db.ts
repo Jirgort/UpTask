78f987280aa6b333f7cd3c27cbe526f8fe4a43ec
Jirgort <Jirgort@gmail.com>
2024-04-06 23:09:56 -0600

    Connection with database mongoDB

---
+++ upTask_Backend/src/config/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import colors, { bold, magenta } from "colors"
+import {exit} from "node:process"
+
+export const connectDB = async () => {
+    try {
+        const {connection} = await mongoose.connect(process.env.DATABASE_URL)
+        const url = `${connection.host}: ${connection.port}`
+        console.log(colors.magenta.bold(`MongoDB connection in: ${url}`))
+    } catch (error) {
+        //console.log(error.message)
+        console.log(colors.red.bold("Connection error in database"))
+        
+        exit(1)
+    }
+
+}
+
+
